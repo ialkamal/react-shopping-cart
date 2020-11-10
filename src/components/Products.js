ccc4b04169a01e71a8cb9fc6b03bfd8624030fd3
Ismail Al Kamal <ismail@nawatt.com>
2020-11-10 22:38:05 +0300

    Assignment Completed

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, { useContext } from "react";
 
 // Components
-import Product from './Product';
+import Product from "./Product";
 
-const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+import { ProductContext } from "../contexts/ProductContext";
+
+const Products = () => {
+  const value = useContext(ProductContext);
+  return (
+    <div className="products-container">
+      {value.products.map((product) => (
+        <Product key={product.id} product={product} addItem={value.addItem} />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
